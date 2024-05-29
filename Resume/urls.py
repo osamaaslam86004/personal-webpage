cafb544f24c28df3fcd33ce7389dd50ee30670ce
osamaaslam86004 <osama.aslam.86004@gmail.com>
2024-05-29 18:33:36 +0500

    redirectview modified

--- Resume/urls.py
+++ Resume/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from Resume.views import RenderResume
-from django.views.generic import RedirectView
 
 
 urlpatterns = [
-    path("", RedirectView.as_view(pattern_name="render-resume", permanent=True)),
     path("intern-python-web-developer/", RenderResume.as_view(), name="render-resume"),
 ]
--- personal_webpage/urls.py
+++ personal_webpage/urls.py
@@ -19,9 +19,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("resume/", include("Resume.urls")),
     path("cover-letter/", include("Cover_Letter.urls")),
+    path("", RedirectView.as_view(pattern_name="render-resume", permanent=True)),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
