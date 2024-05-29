58b43b912583f3853ba9f3ff6c082d30fe2604d0
osamaaslam86004 <osama.aslam.86004@gmail.com>
2024-05-29 18:27:00 +0500

    redirectview

--- Resume/urls.py
+++ Resume/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from Resume.views import RenderResume
+from django.views.generic import RedirectView
 
 
 urlpatterns = [
-    path("intern-python-web-developer/", RenderResume.as_view()),
+    path("", RedirectView.as_view(pattern_name="render-resume", permanent=True)),
+    path("intern-python-web-developer/", RenderResume.as_view(), name="render-resume"),
 ]
--- Resume/views.py
+++ Resume/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
 
 
 class RenderResume(TemplateView):
