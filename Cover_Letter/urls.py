e9dee69515d8a9b5db5d0f8bd44f3e67cb64c7da
osamaaslam86004 <osama.aslam.86004@gmail.com>
2024-05-29 19:45:23 +0500

    cover letter redirect

--- Cover_Letter/urls.py
+++ Cover_Letter/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
 from Cover_Letter.views import RenderCoverLetter
-
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path(
         "For-Entry-level-Django-Web-Developer/",
         RenderCoverLetter.as_view(),
+        name="Entry-level-Django-Web-Developer",
+    ),
+    path(
+        "",
+        RedirectView.as_view(
+            pattern_name="For-Entry-level-Django-Web-Developer", permanent=True
+        ),
     ),
 ]
