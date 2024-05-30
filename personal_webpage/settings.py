9f364d5c79ce9e3a9e3a756716c514393068c0bd
osamaaslam86004 <osama.aslam.86004@gmail.com>
2024-05-30 14:56:41 +0500

    commit

--- personal_webpage/settings.py
+++ personal_webpage/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = "personal_webpage.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ["templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
