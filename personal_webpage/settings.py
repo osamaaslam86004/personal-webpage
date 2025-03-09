262807644a3bdd57942701feece7fe55cdd0a820
osamaaslam86004 <osama.aslam.86004@gmail.com>
2025-03-09 05:57:39 +0500

    1. outermost container is responsive with container-lg 2. accordion heading strong 3. accordion list is responsive clamp 4. paddingad ajusted for .content container

--- personal_webpage/settings.py
+++ personal_webpage/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     "csp.middleware.CSPMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.gzip.GZipMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
