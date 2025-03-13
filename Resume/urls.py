9f8dba21109aa15f6fc7b551843e20789b164d6e
osamaaslam86004 <osama.aslam.86004@gmail.com>
2025-03-14 04:30:34 +0500

    typo fixed in view name

--- Resume/urls.py
+++ Resume/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from django.views.generic import RedirectView
 
-from Resume.views import RenderResume
+from Resume.views import render_resume
 
 app_name = "Resume"
 
 urlpatterns = [
-    path("python-django-web-developer/", RenderResume.as_view(), name="render-resume"),
+    path("python-django-web-developer/", render_resume(), name="render-resume"),
     path("", RedirectView.as_view(pattern_name="Resume:render-resume", permanent=True)),
 ]
--- Resume/views.py
+++ Resume/views.py
@@ -5,7 +5,7 @@ from django.views.decorators.cache import cache_control
 
 
 @cache_control(public=True, max_age=604800)  # Cache for 7 days
-def custom_get(request):
+def render_resume(request):
     """Fastest function-based view for rendering the resume with caching & ETag."""
 
     response = render(request, "cv.html")  # Directly render template
