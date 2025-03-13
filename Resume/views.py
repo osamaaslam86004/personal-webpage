ecbf828474f73404ffd0e97ee406833fb186126d
osamaaslam86004 <osama.aslam.86004@gmail.com>
2025-03-14 03:10:25 +0500

    cache header added

--- Resume/views.py
+++ Resume/views.py
@@ -1,5 +1,8 @@
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_control
 from django.views.generic import TemplateView
 
 
+@method_decorator(cache_control(public=True, max_age=604800), name="dispatch")
 class RenderResume(TemplateView):
     template_name = "cv.html"
