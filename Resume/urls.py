24296d2a7f6a065a053c2c3bc65e879b9af741b9
osamaaslam86004 <osama.aslam.86004@gmail.com>
2025-03-14 04:34:12 +0500

    typo fixed

--- Resume/urls.py
+++ Resume/urls.py
@@ -6,6 +6,6 @@ from Resume.views import render_resume
 app_name = "Resume"
 
 urlpatterns = [
-    path("python-django-web-developer/", render_resume(), name="render-resume"),
+    path("python-django-web-developer/", render_resume, name="render-resume"),
     path("", RedirectView.as_view(pattern_name="Resume:render-resume", permanent=True)),
 ]
