196ace9dea894f5f7fce11af019b26ac09976fad
osamaaslam86004 <osama.aslam.86004@gmail.com>
2025-03-14 03:21:03 +0500

    etag header added with fast and lightweight hashing """ 🚀 Why This is Better ✔ Consistent across requests (Unlike hash()) ✔ MD5 is fast and lightweight   (Use sha256 for extra security) ✔ Prevents unnecessary re-downloads   (If content hasn’t changed)
    If security is a concern, use SHA-256 instead of MD5:
    """

--- Resume/views.py
+++ Resume/views.py
@@ -1,3 +1,5 @@
+import hashlib
+
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_control
 from django.views.generic import TemplateView
@@ -6,3 +8,9 @@ from django.views.generic import TemplateView
 @method_decorator(cache_control(public=True, max_age=604800), name="dispatch")
 class RenderResume(TemplateView):
     template_name = "cv.html"
+
+    def render_to_response(self, context, **response_kwargs):
+        response = super().render_to_response(context, **response_kwargs)
+        content_hash = hashlib.md5(response.content).hexdigest()  # Fast and reliable
+        response["ETag"] = f'W/"{content_hash}"'  # Weak ETag
+        return response
